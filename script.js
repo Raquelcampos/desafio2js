8b66fc840bbd53c23e5e95a82dcd33d6afdde66c
Raquelcampos <martinsraquelcampos@gmail.com>
2022-06-11 13:54:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+let idade = 22;
+if(idade >18){
+    console.log('você é maior de idade')
+}
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+let verificar = 18 <= 22;
+console.log(verificar)
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+let mesAniversario = "Janeiro";
+if((mesAniversario === "Janeiro") || (mesAniversario === "Dezembro")){
+    console.log("Você faz aniversário no verão");
+}else{
+    console.log("'infelizemnte', não foi dessa vez")
+}
+
+
+//*** Exercícios bônus
+//4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+//5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
+
+
